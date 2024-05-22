7a7fbb552ea40d30f5655993a6f0bd3e236b46b4
Antonello Lobianco <antonello@lobianco.org>
2024-05-22 16:13:44 +0200

    Corrected test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,16 +2,4 @@ using Test, MrFOR_simrep
 
 def_settings = load_settings()
 @test def_settings["simulation_region"]["cres_epsg_id"] == 3035
-
-a = "\${SCENARIO_PATH}/forest_types.txt"
-replace!(a,"\${SCENARIO_PATH}" => "/some/path")
-
-adic = Dict("a"=>1,"b"=>2,"c"=>3)
-
-plusone(x) = x+1
-
-map((k,v) -> plusone(v), adic)
-
-for (k,v) in adic
-    plusone(v)
-end
+@test isfile(def_settings["ft_list"])
