1fa16b55d2f6693b890bba1d6c6a30d78cfcfb3f
Antonello Lobianco <antonello@lobianco.org>
2024-05-22 16:09:39 +0200

    Added more settings for resource model and recursive_replace! funcitons for relative path names

--- src/MrFOR_simrep.jl
+++ src/MrFOR_simrep.jl
@@ -24,6 +24,16 @@ export load_settings
 #dicts2nt(x) = x
 #dicts2nt(d::Dict) = (; (Symbol(k) => dicts2nt(v) for (k, v) in d)...)
 
+recursive_replace!(obj,replacement_pattern) = obj
+recursive_replace!(obj::AbstractArray,replacement_pattern)  = recursive_replace!.(obj::AbstractArray,Ref(replacement_pattern))
+recursive_replace!(obj::AbstractString,replacement_pattern) = replace(obj,replacement_pattern)
+function recursive_replace!(obj::AbstractDict,replacement_pattern)
+    for (k,v) in obj
+        obj[k] = recursive_replace!(v,replacement_pattern)
+    end
+    return obj
+end
+
 """
     load_settings(project="default",scenario="default")
 
@@ -31,11 +41,13 @@ Load the settings for a specific project and scenario.
 The settings are returned as a dictionary.
 """
 function load_settings(project="default",scenario="default")
-    rep_path = joinpath(@__DIR__,"..","repository")
-    project_path = joinpath(rep_path,project)
+    rep_path      = joinpath(@__DIR__,"..","repository")
+    project_path  = joinpath(rep_path,project)
     scenario_path = joinpath(project_path,"scenarios",scenario)
     settings_path = joinpath(scenario_path,"settings.yaml")
-    settings = YAML.load_file(settings_path)
+    ENV["SCENARIO_PATH"] = scenario_path
+    settings      = YAML.load_file(settings_path)
+    recursive_replace!(settings,"\${SCENARIO_PATH}" => scenario_path)
     return settings
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,16 @@ using Test, MrFOR_simrep
 
 def_settings = load_settings()
 @test def_settings["simulation_region"]["cres_epsg_id"] == 3035
+
+a = "\${SCENARIO_PATH}/forest_types.txt"
+replace!(a,"\${SCENARIO_PATH}" => "/some/path")
+
+adic = Dict("a"=>1,"b"=>2,"c"=>3)
+
+plusone(x) = x+1
+
+map((k,v) -> plusone(v), adic)
+
+for (k,v) in adic
+    plusone(v)
+end
